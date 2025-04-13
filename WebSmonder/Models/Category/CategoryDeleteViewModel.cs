198672bbcafad47c248726a4cf0a8f0c94621875
0Taras0 <rringoo043@gmail.com>
2025-04-13 21:41:58 +0300

    Modified delete page

--- WebSmonder/Models/Category/CategoryDeleteViewModel.cs
+++ WebSmonder/Models/Category/CategoryDeleteViewModel.cs
@@ -5,8 +5,12 @@ namespace WebSmonder.Models.Category
     public class CategoryDeleteViewModel
     {
         public int Id { get; set; }
+        [Display(Name = "Назва категорії:")]
         public string Name { get; set; } = string.Empty;
+
+        [Display(Name = "Опис:")]
         public string? Description { get; set; } = string.Empty;
+        [Display(Name = "Url адреса фото:")]
         public string ImageUrl { get; set; } = string.Empty;
     }
 }
